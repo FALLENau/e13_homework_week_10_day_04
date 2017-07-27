1fea900cd98f50c8e365f7b4c476561bbecf0601
FALLENau <reece.c.jones@gmail.com>
2017-07-27 11:38:32 +0100

    reeal world

--- closures/closures.js
+++ closures/closures.js
@@ -1,7 +1,23 @@
-var setupAddFunction = function() {
+var setupAddFunction = function(modifier) {
   var counter = 0
   return function() {
-    counter++
-    console.log(conter)
+    counter += modifier
+    console.log(counter)
   }
 }
+
+var addFive = setupAddFunction(5)
+console.log("The value of add is: ", addFive)// prints function out
+
+addFive()//logs out 1
+addFive()//logs out 2
+addFive()//logs out 3
+
+// console.log(add())//logs out undifined becuz var counter doesn't
+//exist out side of function
+
+var addTen = setupAddFunction(10)
+
+addTen()
+addTen()
+addTen()
