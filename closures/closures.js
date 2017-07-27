45a7e3144707693ede3b0ccf0390d5bde492a82c
FALLENau <reece.c.jones@gmail.com>
2017-07-27 11:45:55 +0100

    person scope

--- closures/closures.js
+++ closures/closures.js
@@ -1,23 +1,32 @@
-var setupAddFunction = function(modifier) {
-  var counter = 0
-  return function() {
-    counter += modifier
-    console.log(counter)
-  }
-}
+// var setupAddFunction = function(modifier) {
+//   var counter = 0
+//   return function() {
+//     counter += modifier
+//     console.log(counter)
+//   }
+// }
 
-var addFive = setupAddFunction(5)
-console.log("The value of add is: ", addFive)// prints function out
-
-addFive()//logs out 1
-addFive()//logs out 2
-addFive()//logs out 3
+// var addFive = setupAddFunction(5)
+// console.log("The value of add is: ", addFive)// prints function out
+//
+// addFive()//logs out 1
+// addFive()//logs out 2
+// addFive()//logs out 3
 
 // console.log(add())//logs out undifined becuz var counter doesn't
 //exist out side of function
 
-var addTen = setupAddFunction(10)
+// var addTen = setupAddFunction(10)
+//
+// addTen()
+// addTen()
+// addTen()
 
-addTen()
-addTen()
-addTen()
+var addMessage = setupConversation() {
+
+  
+}
+
+addMessage("I've got something very important to tell you that must never be forgotten");
+addMessage("Oh yeah?");
+addMessage("Oh, no. Wait... It's fine.");
